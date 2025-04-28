41ca1c86d685ac9d568e019639c08ed9eb64447c
Snehith7781 <2100032096@kluniversity.in>
2025-04-28 15:08:11 +0530

    Initial commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('This is the 2nd project');
+  res.send('This is the 2nd project capstone-2');
   res.send('This is alternate text');
 });
 
